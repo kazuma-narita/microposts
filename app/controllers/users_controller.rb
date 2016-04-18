a743c34d43c65ed1028ee16706027bb407669103
kazuma <kazuma-narita@united.jp>
2016-04-18 04:41:19 +0000

    profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  
   def show # 追加
-   @user = User.find(params[:id])
   end
   
   def new
@@ -17,10 +19,28 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      flash[:success] = "更新に成功しました"
+      redirect_to @user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
+  private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location, :profile)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
