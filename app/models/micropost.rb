bd8c71501cb2e565ba205014a2ab9abc2b10130b
kazuma <kazuma-narita@united.jp>
2016-04-18 07:08:19 +0000

    fource commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,3 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-<<<<<<< HEAD
-  validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
 end
-=======
-end
->>>>>>> user-profile
